1d15185a8a14dbfdf9be10b0e8128f473c150da3
Elias Wachmann <elias.wachmann@gmail.com>
2020-03-09 13:38:30 +0100

    working 10/10

--- Introduction/Week2/grader.py
+++ Introduction/Week2/grader.py
@@ -1,5 +1,14 @@
 from math import tan, pi
-n = 4
-s = 1
-area = (0.25*n*s**2)/(tan(pi/n))
-print(area)
+
+
+def polysum(n, s):
+    """takes sides and length of sides from polygon and sums the area with the
+        squared perimeter
+    """
+    area = (0.25*n*s**2)/(tan(pi/n))
+    peri = n*s
+    ans = area + peri**2
+    return round(ans, 4)
+
+
+print(polysum(3, 15))
