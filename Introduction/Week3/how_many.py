0d15322d5e1e1ba5b0ace8e6690508536e6d6936
Elias Wachmann <elias.wachmann@gmail.com>
2020-03-10 10:41:30 +0100

    with sum

---
+++ Introduction/Week3/how_many.py
@@ -0,0 +1,12 @@
+animals = {'a': ['aardvark'], 'b': ['baboon'], 'c': ['coati']}
+
+animals['d'] = ['donkey']
+animals['d'].append('dog')
+animals['d'].append('dingo')
+
+
+def how_many(aDict):
+    print(sum(len(v) for v in aDict.values()))
+
+
+how_many(animals)
