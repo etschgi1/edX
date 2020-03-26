b2c0ce51241bac0b11cc93d58943c604fc5bbd33
Elias Wachmann <elias.wachmann@gmail.com>
2020-03-26 18:10:07 +0100

    finishing 6.00X1

--- Introduction/Week7/plotting_example.py
+++ Introduction/Week7/plotting_example.py
@@ -16,11 +16,16 @@ for i in range(1, 15):
     yexp.append((math.e)**i)
 
 plots = [ylin, yquad, ylog, ynlog]
+plotnames = ['lin', 'quad', 'log', 'nlog']
+i = 0
 for plot in plots:
-    plt.figure("Vergleich lin,quad,log,nlog")
-    plt.plot(xvals, plot)
+    plt.figure(1)
+    plt.plot(xvals, plot, label=str(plotnames[i]))
     plt.xlabel('x-Werte')
     plt.ylabel('y-Werte')
-    plt.title('Testtitle')
+    plt.title('Test')
+    plt.legend(loc='upper right')
+    i += 1
+    plt.ylim(0, 20)
 plt.yscale('linear')
 plt.show()
