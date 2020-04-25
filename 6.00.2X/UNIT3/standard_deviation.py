e7906d2a0c2745d3be51e28fe6f9b5207e1be339
Elias Wachmann <elias.wachmann@gmail.com>
2020-04-25 11:19:28 +0200

    added statistic lib function and comparision using timetrial. Also added stdDevOfLengths for int or float lists

--- 6.00.2X/UNIT3/standard_deviation.py
+++ 6.00.2X/UNIT3/standard_deviation.py
@@ -1,4 +1,23 @@
-sample = ["tes", "miauh", "foooooo"]
+import statistics
+import math
+import time
+sample = ["tes", "miauh", "foooooo", "tes", "miauh", "foooooo", "tes", "miauh", "foooooo", "tes",
+          "miauh", "foooooo", "tes", "miauh", "foooooo", "tes", "miauh", "foooooo", "tes", "miauh", "foooooo", ]
+
+n = [10, 4, 12, 15, 20, 5]
+
+
+def stddevOflist(s):
+    """
+    s: sample inputs ints or floats
+    returns standard deviation
+    """
+    if not s:
+        return math.nan
+    return round(statistics.pstdev(s), 4)
+
+
+print(stddevOflist(n)/(sum(n)/len(n)))
 
 
 def stdDevOfLengths(s):
@@ -7,6 +26,7 @@ def stdDevOfLengths(s):
     returns a float, the sandard deviation of the lengths of strings in list,
     NaN if L is empty.
     """
+    # time.sleep(0.01)
     if not s:
         return float('nan')
     # make list of individual sample lenght
@@ -19,4 +39,28 @@ def stdDevOfLengths(s):
     return (tot/len(lengths))**0.5  # standard deviation
 
 
-print(stdDevOfLengths(sample))
+def stdUsingMath(s):
+    """s: a List of sample inputs
+    returns a float, the sandard deviation of the lengths of strings in list,
+    NaN if L is empty. Using math lib
+    """
+    # time.sleep(0.01)
+    if not s:
+        return math.nan
+    return statistics.pstdev([len(i) for i in s])
+
+
+# timelist = []
+# timemat = []
+
+# for i in range(100000):
+#     start = time.time()
+#     print(stdDevOfLengths(sample))
+#     timelist.append(time.time()-start)
+#     start = time.time()
+#     print(stdUsingMath(sample))
+#     timemat.append(time.time()-start)
+
+# print("-----------------")
+# print(f"time list: {sum(timelist)/len(timelist)}")
+# print(f"time math: {sum(timemat)/len(timemat)}")
